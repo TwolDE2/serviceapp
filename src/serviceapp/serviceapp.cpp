b61cb90376125931df5184223153b965aa750454
Twol <tonywhalley@t-online.de>
2022-06-13 15:57:37 +0200

    serviceapp - try 150 msec timeout value

--- src/serviceapp/serviceapp.cpp
+++ src/serviceapp/serviceapp.cpp
@@ -878,7 +878,7 @@ RESULT eServiceApp::isCurrentlySeekable()
 int eServiceApp::getNumberOfTracks()
 {
 	eDebug("eServiceApp::getNumberOfTracks");
-	return player->audioGetNumberOfTracks(250);
+	return player->audioGetNumberOfTracks(150);
 }
 
 RESULT eServiceApp::selectTrack(unsigned int i)
@@ -1056,7 +1056,7 @@ RESULT eServiceApp::getSubtitleList(std::vector<struct SubtitleTrack> &subtitlel
 {
 	m_subtitle_tracks.clear();
 	m_subtitle_streams.clear();
-	int embedded_track_num = player->subtitleGetNumberOfTracks(250);
+	int embedded_track_num = player->subtitleGetNumberOfTracks(150);
 	eDebug("eServiceApp::getSubtitleList - found embedded tracks (%d)", embedded_track_num);
 	int pid = 0;
 	for (; pid < embedded_track_num; pid++)
