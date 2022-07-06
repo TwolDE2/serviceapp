a909df891be20ee93a3068ce79c62818c7356495
Twol <tonywhalley@t-online.de>
2022-07-06 10:15:31 +0200

    yconole correct logic

--- src/serviceapp/myconsole.cpp
+++ src/serviceapp/myconsole.cpp
@@ -34,11 +34,11 @@ int bidirpipe(int pfd[], const char *cmd , const char * const argv[], const char
 		if ( close(0) == -1 || close(1) == -1 || close(2) == -1 )
 			eDebug("[ServiceApp][eConsoleContainer][bidirpipe]3 close 0,1,2 exit");		
 			_exit(0);
-		duppfdin == dup(pfdin[1]);
-		duppfdout == dup(pfdout[0]);
-		duppfderr == dup(pfderr[1]);
+		duppfdin = dup(pfdin[1]);
+		duppfdout = dup(pfdout[0]);
+		duppfderr = dup(pfderr[1]);
 		eDebug("[ServiceApp][eConsoleContainer][bidirpipe]4 dup in = %d, out = %d, err = %d", duppfdin, duppfdout, duppfderr);		
-		if duppfdout != 0 || duppfdin != 1 || duppfderr != 2 )
+		if (duppfdout != 0 || duppfdin != 1 || duppfderr != 2 )
 			eDebug("[ServiceApp][eConsoleContainer][bidirpipe]5 dup exit");		
 			_exit(0);
 
